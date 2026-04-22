80ef5564abf78ff705c0978a5ef94cfafa808efd
Gabriel0925 <gabchap486@gmail.com>
2026-04-22 19:45:13 +0200

    Fix bugs

--- app-settings.js
+++ app-settings.js
@@ -2,12 +2,8 @@ g.clear();
 
 var menu = {
     "": {"title": "Apps"},
-    // Param de la montre
-    "Screen": function() {console.log("Screen");},
-
     // Information
     "About": function() {about();},
-    "Version": function() {console.log("Version");},
 };
 
 E.showMenu(menu);
@@ -17,10 +13,10 @@ function about() {
     g.setFontAlign(-1,-1);
     g.setFont("Vector", 20);
 
-    g.drawString("v1", 82, 88); 
+    g.drawString("v0 beta 1", 20, 88); 
 }
 
 // Pour détécter quand il y a un appuie sur le bouton
 setWatch(() => {
-    load("main.js");
+    load("launcher.js");
 }, BTN1, {repeat:true, edge: "falling"}); // "falling" : le moment ou tu relaches le bouton et repeat true pour continuer de détecter les clics même si on a deja cliqué sur le bouton
